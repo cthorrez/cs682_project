ac4fac6b42ad71e1f8b9a2966a8a146d4553439e
thorrez <thorrez@umich.edu>
2018-12-09 21:13:07 -0500

    cocat bboxes in right dim

--- models.py
+++ models.py
@@ -90,7 +90,7 @@ class LossNet(nn.Module):
         self.fc2 = nn.Linear(64,1)
 
     def forward(self,bbs, bb_preds):
-        x = torch.cat([bbs, bb_preds])
+        x = torch.cat([bbs, bb_preds], dim=1)
         x = self.fc1(x)
         x = self.relu(x)
         x = self.fc2(x)
